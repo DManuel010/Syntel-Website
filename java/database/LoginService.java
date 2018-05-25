b190a758df6d8c6235c123b3b5a702a845302948
shannonsterbenz <38961742+shannonsterbenz@users.noreply.github.com>
2018-05-24 20:14:35 -0700

    customer registration
    slightly changed getPK methods. customer registration works on console app in a scratch main class

--- java/database/LoginService.java
+++ java/database/LoginService.java
@@ -9,92 +9,116 @@ import java.sql.Statement;
 
 import entities.Login;
 
-public class LoginService extends Service {
-
-	public LoginService(Connection conn) {
-		super(conn);
-	}
-
+public class LoginService {
 	
-	public void insert(Object obj) {
-		Login login = (Login) obj;
-
-		//INSERT INTO TABLE
-		System.out.println("Inserting a new user...");
 		
-		try {
-			PreparedStatement insertStmt = conn.prepareStatement("insert into login values (?,?,?)");
-			insertStmt.setInt(1,login.getLoginID()); 
-			insertStmt.setString(2,login.getUsername()); 
-			insertStmt.setString(3,login.getPassword()); 
-			insertStmt.execute();
-			System.out.println();
-			System.out.println("User added with login ID "+login.getLoginID());
-			insertStmt.close();
-		} catch (SQLException e) {
-			System.out.println("Error: SQL Exception.");
-			e.printStackTrace();
-		}
-	}
+static Connection con;
 
-	private int getPK() {
-		int lastPK = 0;
-		int newPK = 0;
-		String query = "SELECT MAX(loginID) FROM login";
+
+public LoginService(Connection con) {
+	super();
+	this.con=con;
+}
+
+private int getPK() {
+	int lastPK = 0;
+	int newPK = 0;
+	String query = "SELECT MAX(loginID) FROM login";
 	
-		try {
-			Statement statement = this.conn.createStatement();
-			ResultSet result = statement.executeQuery(query);
+	try {
+		Statement statement = this.con.createStatement();
+		ResultSet result = statement.executeQuery(query);
 		
-			while(result.next()) {
-				lastPK = result.getInt("loginID");
-			}
-			newPK = lastPK + 1;
-		} catch (SQLException e) {
-			System.out.println("Failed to connect to database.");
-			e.printStackTrace();
+		while(result.next()) {
+			lastPK = result.getInt(1);
+			
 		}
-		return newPK;
-	}
-
-	
-	public void delete(int loginID) {
-		//DELETE FROM TABLE
-		System.out.println("Deleting user with login ID "+loginID+"...");
+		newPK = lastPK + 1;
 		
-		try{
-			PreparedStatement deleteStmt = conn.prepareStatement("delete from login where loginID=?");
-			deleteStmt.setInt(1,loginID); 
-			deleteStmt.execute();
-			System.out.println();
-			System.out.println("User deleted.");
-			deleteStmt.close();
-		}catch (SQLException e) {
-			System.out.println("Error: SQL Exception.");
-			e.printStackTrace();
-		}
+	} catch (SQLException e) {
+		System.out.println("Failed to connect to database.");
+		e.printStackTrace();
 	}
+	return newPK;
+}
 
+
+public void insert(Login login)
+{
+
+	//INSERT INTO TABLE
+	int loginID;
+	if(login.getLoginID() == 0) {
+		loginID = getPK();
+		login.setLoginID(loginID);
+	}
+	else {
+		loginID = login.getLoginID();
+	}
 	
-	public void display() {
-		//DISPLAY FROM TABLE
+	
+	System.out.println("Inserting a new user...");
+			
+	
+	try {
+		PreparedStatement insertStmt = con.prepareStatement("insert into login values (?,?,?)");
+		insertStmt.setInt(1,loginID); 
+		insertStmt.setString(2,login.getUsername()); 
+		insertStmt.setString(3,login.getPassword()); 
+		insertStmt.execute();
+		System.out.println();
+		System.out.println("User added with login ID "+loginID);
+		insertStmt.close();
+	} catch (SQLException e) {
+		System.out.println("Error: SQL Exception.");
+		e.printStackTrace();
+	}
+	
+}
+
+public void delete(int loginID)
+{
+	//DELETE FROM TABLE
+	System.out.println("Deleting user with login ID "+loginID+"...");
+	
+	try{
+		PreparedStatement deleteStmt = con.prepareStatement("delete from login where loginID=?");
+		deleteStmt.setInt(1,loginID); 
+		deleteStmt.execute();
+		System.out.println();
+		System.out.println("User deleted.");
+		deleteStmt.close();
+	}catch (SQLException e) {
+		System.out.println("Error: SQL Exception.");
+		e.printStackTrace();
+	}
+	
+}
+
+public void display()
+
+{
+	
+	//DISPLAY FROM TABLE
 		System.out.println("Displaying user...");
 		System.out.println("Login ID		Username		Password");
-			
+		
 		try{
-			PreparedStatement oracleStmt = conn.prepareStatement("select * from login");
+			PreparedStatement oracleStmt = con.prepareStatement("select * from login");
 			oracleStmt.execute();
 			ResultSet oracleRs = oracleStmt.getResultSet();
-				
-			while(oracleRs.next()) {
+			
+			while(oracleRs.next())
+			{
 				System.out.println(oracleRs.getInt(1)+"		"+oracleRs.getString(2)+"		"+oracleRs.getString(3));
 			}
 			System.out.println();
 			System.out.println("Users Displayed.");
 			oracleStmt.close();
-		} catch (SQLException e) {
+		}catch (SQLException e) {
 			System.out.println("Error: SQL Exception.");
 			e.printStackTrace();
 		}
+
 	}
 }
