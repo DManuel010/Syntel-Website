7b7e90f14ed638c6389f9f06b988b70e74207cf8
shannonsterbenz <38961742+shannonsterbenz@users.noreply.github.com>
2018-05-28 19:28:36 -0700

    login service
    added view method to get login info from one user in the login table

--- java/database/LoginService.java
+++ java/database/LoginService.java
@@ -9,83 +9,105 @@ import java.sql.Statement;
 
 import entities.Login;
 
-public class LoginService extends Service {
+public class LoginService {
 	
 		
 static Connection con;
 
 
-	public LoginService(Connection conn) {
-		super(conn);
-	}
+public LoginService(Connection con) {
+	super();
+	this.con=con;
+}
 
+private int getPK() {
+	int lastPK = 0;
+	int newPK = 0;
+	String query = "SELECT MAX(loginID) FROM login";
 	
-	@Override
-	public void insert(Object obj) {
+	try {
+		Statement statement = this.con.createStatement();
+		ResultSet result = statement.executeQuery(query);
 		
-		Login login = (Login) obj;
-		
-		//INSERT INTO TABLE
-		int loginID;
-		if(login.getLoginID() == 0) {
-			loginID = getPK(0);
-			login.setLoginID(loginID);
+		while(result.next()) {
+			lastPK = result.getInt(1);
+			
 		}
-		else {
-			loginID = login.getLoginID();
-		}
+		newPK = lastPK + 1;
 		
-		
-		System.out.println("Inserting a new user...");
-				
-		
-		try {
-			PreparedStatement insertStmt = con.prepareStatement("insert into login values (?,?,?)");
-			insertStmt.setInt(1,loginID); 
-			insertStmt.setString(2,login.getUsername()); 
-			insertStmt.setString(3,login.getPassword()); 
-			insertStmt.execute();
-			System.out.println();
-			System.out.println("User added with login ID "+loginID);
-			insertStmt.close();
-		} catch (SQLException e) {
-			System.out.println("Error: SQL Exception.");
-			e.printStackTrace();
-		}
-		
+	} catch (SQLException e) {
+		System.out.println("Failed to connect to database.");
+		e.printStackTrace();
 	}
+	return newPK;
+}
 
-	public void delete(int loginID)
-	{
-		//DELETE FROM TABLE
-		System.out.println("Deleting user with login ID "+loginID+"...");
-		
-		try{
-			PreparedStatement deleteStmt = con.prepareStatement("delete from login where loginID=?");
-			deleteStmt.setInt(1,loginID); 
-			deleteStmt.execute();
-			System.out.println();
-			System.out.println("User deleted.");
-			deleteStmt.close();
-		}catch (SQLException e) {
-			System.out.println("Error: SQL Exception.");
-			e.printStackTrace();
-		}
-		
+
+public void insert(Login login)
+{
+
+	//INSERT INTO TABLE
+	int loginID;
+	if(login.getLoginID() == 0) {
+		loginID = getPK();
+		login.setLoginID(loginID);
 	}
+	else {
+		loginID = login.getLoginID();
+	}
+	
+	
+	System.out.println("Inserting a new user...");
+			
+	
+	try {
+		PreparedStatement insertStmt = con.prepareStatement("insert into login values (?,?,?)");
+		insertStmt.setInt(1,loginID); 
+		insertStmt.setString(2,login.getUsername()); 
+		insertStmt.setString(3,login.getPassword()); 
+		insertStmt.execute();
+		System.out.println();
+		System.out.println("User added with login ID "+loginID);
+		insertStmt.close();
+	} catch (SQLException e) {
+		System.out.println("Error: SQL Exception.");
+		e.printStackTrace();
+	}
+	
+}
 
-	public void display()
-	{
-		
-		//DISPLAY FROM TABLE
+public void delete(int loginID)
+{
+	//DELETE FROM TABLE
+	System.out.println("Deleting user with login ID "+loginID+"...");
+	
+	try{
+		PreparedStatement deleteStmt = con.prepareStatement("delete from login where loginID=?");
+		deleteStmt.setInt(1,loginID); 
+		deleteStmt.execute();
+		System.out.println();
+		System.out.println("User deleted.");
+		deleteStmt.close();
+	}catch (SQLException e) {
+		System.out.println("Error: SQL Exception.");
+		e.printStackTrace();
+	}
+	
+}
+
+public void display()
+
+{
+	
+	//DISPLAY FROM TABLE
 		System.out.println("Displaying user...");
 		System.out.println("Login ID		Username		Password");
-			
+		
 		try{
 			PreparedStatement oracleStmt = con.prepareStatement("select * from login");
 			oracleStmt.execute();
 			ResultSet oracleRs = oracleStmt.getResultSet();
-				
+			
 			while(oracleRs.next())
 			{
 				System.out.println(oracleRs.getInt(1)+"		"+oracleRs.getString(2)+"		"+oracleRs.getString(3));
@@ -97,34 +119,35 @@ static Connection con;
 			System.out.println("Error: SQL Exception.");
 			e.printStackTrace();
 		}
+
 	}
 
-	// increment the primary key for new insertion
-	public int getPK(int loginID) {
-		int lastPK = 0;
-		int newPK = 0;
-		String query = "SELECT MAX(loginID) AS pk " +
-						"FROM Login";
+public boolean view(String username)
+
+{
+	
+	//DISPLAY FROM TABLE
+		System.out.println("Displaying user...");
+		boolean exists = false;
 		
-		try {
-			Statement statement = this.conn.createStatement();
-			ResultSet result = statement.executeQuery(query);
+		try{
+			PreparedStatement oracleStmt = con.prepareStatement("select * from login where username=?");
+			oracleStmt.setString(1, username);
+			oracleStmt.execute();
+			ResultSet oracleRs = oracleStmt.getResultSet();
 			
-			if(result.next()) {
-				lastPK = result.getInt("pk");
+			if(oracleRs.next())
+			{
+				exists=true;
 			}
 			
-			if(loginID <= lastPK) {
-				newPK = lastPK + 1;
-			}
-			else {
-				newPK = loginID;
-			}
+			oracleStmt.close();
+		}catch (SQLException e) {
+			System.out.println("Error: SQL Exception.");
 			
-		} catch (SQLException e) {
-			System.out.println("LoginService:  Failed to get new Primary Key.");
-			e.printStackTrace();
 		}
-		return newPK;
+		
+		return exists;
+
 	}
 }
