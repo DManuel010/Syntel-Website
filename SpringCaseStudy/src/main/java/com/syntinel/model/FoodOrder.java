1c25fb52c6c0ee2859f27ed88728f67e3aed2d7e
Unknown <shannonsterbenz@yahoo.com>
2018-06-06 15:38:30 -0700

    models
    added foodorder and combo model classes

---
+++ SpringCaseStudy/src/main/java/com/syntinel/model/FoodOrder.java
@@ -0,0 +1,50 @@
+package com.syntinel.model;
+
+import javax.validation.constraints.Size;
+
+public class FoodOrder {
+
+	private int foodOrderId;
+	private int orderId;
+	private int foodId;
+	
+	@Size(min=0)
+	private int quantity;
+
+	public FoodOrder() {
+		
+	}
+
+	public int getFoodOrderId() {
+		return foodOrderId;
+	}
+
+	public void setFoodOrderId(int foodOrderId) {
+		this.foodOrderId = foodOrderId;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getFoodId() {
+		return foodId;
+	}
+
+	public void setFoodId(int foodId) {
+		this.foodId = foodId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+}
