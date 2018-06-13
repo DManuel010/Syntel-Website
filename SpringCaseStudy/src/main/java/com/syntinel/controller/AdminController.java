406a59b4daa54154f403d341ad8eb94a2d21cd03
DManuel010 <d.manuel010@gmail.com>
2018-06-13 11:01:46 -0700

    imports and formatting

--- SpringCaseStudy/src/main/java/com/syntinel/controller/AdminController.java
+++ SpringCaseStudy/src/main/java/com/syntinel/controller/AdminController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,10 +13,8 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.syntinel.dao.EmployeeService;
-import com.syntinel.model.Customer;
 import com.syntinel.model.Employee;
 import com.syntinel.validator.AdminValidator;
-import com.syntinel.validator.LoginValidator;
 
 @Controller
 @RequestMapping("/admin")
