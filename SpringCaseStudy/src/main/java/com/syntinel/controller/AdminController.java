49d20b71642cc892478f6a0dbedf14e3cd4d77c2
Jordan Goodman <gojordang@gmail.com>
2018-06-13 09:15:40 -0700

    Minor Changes

--- SpringCaseStudy/src/main/java/com/syntinel/controller/AdminController.java
+++ SpringCaseStudy/src/main/java/com/syntinel/controller/AdminController.java
@@ -1,8 +1,20 @@
 package com.syntinel.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.syntinel.model.Customer;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
+	
+	@RequestMapping(value="/registration", method=RequestMethod.GET)
+	public String loadCustomerForm(ModelMap model)
+	{
+		
+	}
 }
