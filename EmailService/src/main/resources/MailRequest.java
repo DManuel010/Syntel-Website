485c118c6b9a2df504ea4c50fb44cbca9b55a2cc
Unknown <shannonsterbenz@yahoo.com>
2018-06-12 19:36:02 -0700

    basic web service
    starting to create a web service

---
+++ EmailService/src/main/resources/MailRequest.java
@@ -0,0 +1,23 @@
+
+public class MailRequest {
+	
+	private String first_name;
+	private int orderId;
+	
+	public String getFirst_name() {
+		return first_name;
+	}
+	
+	public int getOrderId() {
+		return orderId;
+	}
+	
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+}
